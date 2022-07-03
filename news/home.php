3d532e426852fc8850e3f6ee5ec3bcf150cf2e8f
Ziaullah <mommand.csf@gmail.com>
2022-07-03 13:22:01 +0430

    last commit

--- news/home.php
+++ news/home.php
@@ -5,6 +5,7 @@
 	<?php 
 	  include('../asset.php');
 	  include ('../dbconnect.php');
+	  session_start();
 	?>
 	<style>
 		.hg {
@@ -12,6 +13,10 @@
 		}
 		.pdd{
 			padding-top: 4%;
+		}
+		.error{
+			width: 100% !important;
+			margin-top: 20px !important;
 		}
 	</style>
 </head>
@@ -26,11 +31,41 @@
  	</div>
  	<div class="row">
  		<div class="col-md-6 offset-md-4">
+ 			<?php
+ 			 if (isset($_SESSION['success'])) {
+ 			 	?>
+ 			 	 <div class="row">
+ 			 	 	<div class="alert alert-success">
+ 			 	 		 <?php echo $_SESSION['success'];  ?>
+ 			 	 	</div>
+ 			 	 </div>
+ 			 	<?php
+ 			 	unset($_SESSION['success']);
+ 			 }
+ 			 if (isset($_SESSION['upmsg'])) {
+ 			 	?>
+ 			 		<p class="alert alert-danger error">
+			  	 		<?php echo $_SESSION['upmsg'];  ?>
+			  	 	</p>
+ 			 	<?php
+ 			  unset($_SESSION['upmsg']);
+ 			 }
+ 			?>
  			<div class="row">
- 				<form action="newsController.php" method="post">
+ 				<form action="newsController.php" method="post" enctype="multipart/form-data">
  					<div class="row form-group">
  						News Title
  						<input type="text" name="title" class="form-control">
+ 						<?php
+ 						  if (isset($_SESSION['error'])) {
+ 						  	 ?>
+ 						  	 	<p class="alert alert-danger error">
+ 						  	 		<?php echo $_SESSION['error'];  ?>
+ 						  	 	</p>
+ 						  	 <?php
+ 						  	 unset($_SESSION['error']);
+ 						  }
+ 						?>
  					</div>
  					<div class="row form-group">
  						Category
@@ -51,12 +86,36 @@
 								?>
  							
  						</select>
+ 						<?php
+ 						  if (isset($_SESSION['error1'])) {
+ 						  	 ?>
+ 						  	 	<p class="alert alert-danger error">
+ 						  	 		<?php echo $_SESSION['error1'];  ?>
+ 						  	 	</p>
+ 						  	 <?php
+ 						  	 unset($_SESSION['error1']);
+ 						  }
+ 						?>
+ 					</div>
+ 					<div class="row form-group">
+ 						<p>Upload Photo</p>
+ 						<input type="file" name="image" class="form-control">
  					</div>
  					<div class="row form-group">
  						<p>News Content</p>
  						<textarea class="form-control hg" name="content">
  							
  						</textarea>
+ 						<?php
+ 						  if (isset($_SESSION['error3'])) {
+ 						  	 ?>
+ 						  	 	<p class="alert alert-danger error">
+ 						  	 		<?php echo $_SESSION['error3'];  ?>
+ 						  	 	</p>
+ 						  	 <?php
+ 						  	 unset($_SESSION['error3']);
+ 						  }
+ 						?>
  					</div>
  					<div class="row form-group">
  						<input type="submit" name="" value="Save" class="btn btn-primary">
